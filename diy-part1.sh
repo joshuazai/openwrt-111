394553634e842d185e04c7c5d22def3bc0fd4358
joshuazai <58354020+joshuazai@users.noreply.github.com>
2022-05-15 15:37:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -86,7 +86,7 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-udpxy package/
 # NAS
 
 #文件助手
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
 
 ##############################################################################################
 
